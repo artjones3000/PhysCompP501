ea5004f36005e5def8caaf98393e4d7a7eb4508b
artjones3000 <artjones3000@gmail.com>
2016-09-27 22:01:57 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  
+  fill(255,0,0);
+  rect(10,10,30,70);
+   rect(20,50,30,70);
+  line(20,60, 1,5);
+  fill(123,67,87);
+  ellipse(width/2,height/2,90,80);
+  
+  
+  
+}
